1c8e7608a80fb40e7290a3eb2ddd147b370d82b1
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-03-19 11:29:40 +0100

    Add DateType

---
+++ Planner.Logic/Appointments/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using Planner.LogicInterfaces.Appointments;
+using System;
+
+namespace Planner.Logic.Appointments {
+    public class Appointment : IAppointment {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Finished { get; set; }
+
+        public bool UpdateAppointment() {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAppointment() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Planner.Logic/Appointments/AppointmentContainer.cs
@@ -0,0 +1,22 @@
+﻿using Planner.LogicInterfaces;
+using Planner.LogicInterfaces.Appointments;
+using System;
+using System.Collections.Generic;
+
+namespace Planner.Logic.Appointments {
+    public class AppointmentContainer : IAppointmentContainer {
+        public List<IAppointment> GetAppointments(DateTime date, DateType dateType) {
+            DateTime start = date.GetStart(dateType);
+            DateTime end = date.GetEnd(dateType);
+            throw new NotImplementedException();
+        }
+
+        public IAppointment GetAppointmentById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateAppointment(IAppointment appointment) {
+            throw new NotImplementedException();
+        }
+    }
+}
