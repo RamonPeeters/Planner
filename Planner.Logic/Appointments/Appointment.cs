148a59121a90b43572df9063459120ca9445897e
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-03-26 14:16:54 +0100

    Update appointments to the new structure

--- Planner.Logic/Appointments/Appointment.cs
+++ Planner.Logic/Appointments/Appointment.cs
@@ -2,23 +2,10 @@
 
 namespace Planner.Logic.Appointments {
     public class Appointment {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public bool Finished { get; set; }
-
-        public bool CreateAppointment(Appointment appointment) {
-            throw new NotImplementedException();
-        }
-
-        public bool UpdateAppointment() {
-            throw new NotImplementedException();
-        }
-
-        public bool DeleteAppointment() {
-            throw new NotImplementedException();
-        }
     }
 }
---
+++ Planner.Logic/Appointments/AppointmentCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Planner.Logic.Appointments {
+    public class AppointmentCollection {
+        private readonly List<Appointment> Appointments;
+
+        public AppointmentCollection() {
+            Appointments = new List<Appointment>();
+        }
+
+        public Appointment this[int index] { get { return Appointments[index]; } set { Appointments[index] = value; } }
+
+        public void Add(Appointment appointment) {
+            Appointments.Add(appointment);
+        }
+
+        public bool Remove(Appointment appointment) {
+            return Appointments.Remove(appointment);
+        }
+    }
+}
