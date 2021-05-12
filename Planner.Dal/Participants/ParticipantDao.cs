bfef2eb796f524a4689528997032466c7c146f17
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-05-12 22:04:14 +0200

    Add ParticipantDao and ParticipantDto

---
+++ Planner.Dal/Participants/ParticipantDao.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using Planner.DalInterfaces.Participants;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Planner.Dal.Participants {
+    public class ParticipantDao : IParticipantDao {
+        private readonly string ConnectionString;
+
+        public ParticipantDao(string connectionString) {
+            ConnectionString = connectionString;
+        }
+
+        public List<ParticipantDto> GetParticipants() {
+            using IDbConnection connection = new MySqlConnection(ConnectionString);
+            string sql = "select * from participants;";
+            return connection.Query<ParticipantDto>(sql).ToList();
+        }
+
+        public int CreateParticipant(ParticipantDto participantDto) {
+            using IDbConnection connection = new MySqlConnection(ConnectionString);
+            string sql = "insert into participants(`FirstName`, `LastName`, `Email`) values (@FirstName, @LastName, @Email);";
+            return connection.Execute(sql, new { participantDto.FirstName, participantDto.LastName, participantDto.Email });
+        }
+
+        public int UpdateParticipant(string email, ParticipantDto participantDto) {
+            using IDbConnection connection = new MySqlConnection(ConnectionString);
+            string sql = "update participants set `FirstName` = @FirstName, `LastName` = @LastName, `Email` = @Email where `Email` = @OriginalEmail;";
+            return connection.Execute(sql, new { participantDto.FirstName, participantDto.LastName, participantDto.Email, OriginalEmail = email });
+        }
+
+        public int DeleteParticipant(string email) {
+            using IDbConnection connection = new MySqlConnection(ConnectionString);
+            string sql = "delete from participants where `Email` = @Email;";
+            return connection.Execute(sql, new { Email = email });
+        }
+    }
+}
---
+++ Planner.DalInterfaces/Participants/IParticipantDao.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Planner.DalInterfaces.Participants {
+    public interface IParticipantDao {
+        List<ParticipantDto> GetParticipants();
+        int CreateParticipant(ParticipantDto participantDto);
+        int UpdateParticipant(string email, ParticipantDto participantDto);
+        int DeleteParticipant(string email);
+    }
+}
